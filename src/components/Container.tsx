82ea62306c7814f11a4a65da2890d92ef6b71845
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-03-11 21:22:39 +0900

    Add Container component

---
+++ src/components/Container.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren } from "react";
+
+interface IProps extends PropsWithChildren {
+  className?: string;
+}
+
+const Container = ({ children, className }: IProps) => {
+  return (
+    <div className="max-w-[1304px] px-8 mx-auto w-full overflow-hidden transition-all max-lg:px-5 max-md:px-4">
+      {className ? <div className={className}>{children}</div> : children}
+    </div>
+  );
+};
+
+export default Container;
