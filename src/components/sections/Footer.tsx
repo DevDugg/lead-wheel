43c83202665c571bfde028b14fa6dbecd3197600
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-03-21 00:50:19 +0900

    Add animated in-view effect to footer links

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,17 +1,42 @@
+import AnimatedInView from "../elements/animated-in-view";
 import Container from "../layout/container";
 import FooterLink from "../elements/footer-link";
+import { link } from "fs";
+
+const footerLinks = [
+  {
+    name: "Twitter",
+    href: "#",
+  },
+  {
+    name: "leighlinr@gmail.com",
+    href: "#",
+  },
+];
 
 const Footer = () => {
   return (
     <footer className="footer">
       <Container className="flex items-end justify-between gap-10 pb-[100px]">
         <div className="flex flex-col gap-2">
-          <h6 className="text-2xl leading-NORMAL">Leighlin Ramsay</h6>
-          <p className="text-GRAY leading-NORMAL text-lg">© Lead Wheel. All rights reserved.</p>
+          <AnimatedInView initialVariant={{ y: 20, opacity: 0.2 }} inViewVariant={{ y: 0, opacity: 1 }} threshold={0.4}>
+            <h6 className="text-2xl leading-NORMAL">Leighlin Ramsay</h6>
+          </AnimatedInView>
+          <AnimatedInView initialVariant={{ y: 20, opacity: 0.2 }} inViewVariant={{ y: 0, opacity: 1 }} threshold={0.4}>
+            <p className="text-GRAY leading-NORMAL text-lg">© Lead Wheel. All rights reserved.</p>
+          </AnimatedInView>
         </div>
         <div className="flex items-center gap-10">
-          <FooterLink href="#" name="Twitter" />
-          <FooterLink href="#" name="leighlinr@gmail.com" />
+          {footerLinks.map((link, index) => (
+            <AnimatedInView
+              initialVariant={{ y: 20, opacity: 0.2 }}
+              inViewVariant={{ y: 0, opacity: 1 }}
+              threshold={(index + 4) / 10}
+              key={index}
+            >
+              <FooterLink {...link} />
+            </AnimatedInView>
+          ))}
         </div>
       </Container>
     </footer>
