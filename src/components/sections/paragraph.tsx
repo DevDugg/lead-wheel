871d32614a114d304311dde960d0e9bf35397bdc
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-03-20 12:50:58 +0900

    section removed temporarily

--- src/components/sections/paragraph.tsx
+++ src/components/sections/paragraph.tsx
@@ -1,16 +1,17 @@
 import AnimatedParagraph from "../elements/animated-paragraph";
 import Container from "../layout/container";
-import Section from "../layout/section";
+
+// import Section from "../layout/section";
 
 const Paragraph = () => {
   return (
-    <Section order={1}>
-      <section className="paragraph" id="paragraph">
-        <Container>
-          <AnimatedParagraph text="We don't just promise results. We deliver tangible growth for your agency. Imagine adding an extra 2-5K Monthly Recurring Revenue to your bottom line within a span of 90 days - that's our commitment." />
-        </Container>
-      </section>
-    </Section>
+    // <Section order={1}>
+    <section className="paragraph" id="paragraph">
+      <Container>
+        <AnimatedParagraph text="We don't just promise results. We deliver tangible growth for your agency. Imagine adding an extra 2-5K Monthly Recurring Revenue to your bottom line within a span of 90 days - that's our commitment." />
+      </Container>
+    </section>
+    // </Section>
   );
 };
 export default Paragraph;
